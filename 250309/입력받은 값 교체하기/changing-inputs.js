9490426755f99291056f0ca1564aba62f1189861
864eun <eun00864@gmail.com>
2025-03-09 16:21:41 +0900

    Title: 입력받은 값 교체하기, Time: 162ms, Memory: 15MB, Status: Passed - CodeTree

--- 250309/입력받은 값 교체하기/changing-inputs.js
+++ 250309/입력받은 값 교체하기/changing-inputs.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
-let a = fs.readFileSync(0).toString().trim().split('');
-
-console.log(Number(a[1] a[0]))
+let number = fs.readFileSync(0).toString().trim().split(' ');
+let a = number[0];
+let b = number[1];
+console.log(b + ' '+a)
