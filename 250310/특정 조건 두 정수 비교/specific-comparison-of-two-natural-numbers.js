ffe87881244e8d10d2e457793ff59dde09d22297
864eun <eun00864@gmail.com>
2025-03-10 18:06:13 +0900

    Title: 특정 조건 두 정수 비교, Time: 181ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250310/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ');
+
+let A = Number(input[0]);
+let B = Number(input[1]);
+let C = 0;
+let D = 0;
+
+if(A < B) C=1;
+else C=0;
+
+if(A === B) D=1;
+else D=0;
+
+console.log(C, D)
