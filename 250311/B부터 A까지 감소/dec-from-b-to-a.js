5a8f2d990f9a04ca20702962448c2431f17c1314
864eun <eun00864@gmail.com>
2025-03-11 17:53:48 +0900

    Title: B부터 A까지 감소, Time: 165ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250311/B부터 A까지 감소/dec-from-b-to-a.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(' ');
+let A = Number(input[0]);
+let B = Number(input[1]);
+
+let result = "";
+for(let i=B; i>=A; i--){
+
+    result += (B + ' ');
+    B--;
+}
+console.log(result);
